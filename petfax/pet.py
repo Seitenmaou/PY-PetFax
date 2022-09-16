20e523709fbb5569abcea87c9039aa96babed6d9
Euri Tanaka <dev@mugendiary.com>
2022-09-15 18:40:42 -0700

    adding request redirect functionality

--- petfax/pet.py
+++ petfax/pet.py
@@ -7,12 +7,8 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 
 @bp.route('/')
 def index():
-    return render_template('index.html', pets=pets)
+    return render_template('pets/index.html', pets=pets)
 
 @bp.route('/<int:pet_id>/')
 def petDetail(pet_id):
-    return  render_template('details.html', pet=pets[pet_id])
-
-@bp.route('/new/')
-def newFact():
-    return  render_template('new.html')
+    return  render_template('pets/details.html', pet=pets[pet_id])
